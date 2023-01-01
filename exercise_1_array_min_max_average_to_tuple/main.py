75cb3d22ce85ce6cd85e89fd31f62dcf51088640
Flavio Coscarella <flaviodc@live.com.ar>
2023-01-01 17:22:49 -0300

    Adding Exercise 1

---
+++ exercise_1_array_min_max_average_to_tuple/main.py
@@ -0,0 +1,7 @@
+# Given an array, return min, max and average of the numbers
+
+def min_max_average(array_numbers):
+    return tuple([min(array_numbers), max(array_numbers), sum(array_numbers) / len(array_numbers)])
+
+
+print(min_max_average([4, 8, 1, 25]))
